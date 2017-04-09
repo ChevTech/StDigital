a1f1b427bceefa6b2fec319c77f4db5c3aa10f3b
Python-Student <Anton.Stoytchev@gmail.com>
2017-04-08 20:48:33 -0400

    Default express server

---
+++ server.js
@@ -0,0 +1,10 @@
+var _ = require('lodash');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+	res.send(_.capitalize('hello world'))
+})
+
+app.listen(8080);
