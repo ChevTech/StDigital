fb54a6f1281b8d0ddb27ea12731f0687e2861935
ChevTech <anton.stoytchev@gmail.com>
2018-05-01 23:14:37 -0400

    Prevent codedeploy from hanging forever by closing STDOUT and STDERR #118

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,1 +1,3 @@
-/home/ec2-user/.nvm/versions/node/v8.11.1/bin/node /StDigital/server.js &
+#!/bin/bash -e
+
+/home/ec2-user/.nvm/versions/node/v8.11.1/bin/node /StDigital/server.js >/dev/null 2>&1 &
