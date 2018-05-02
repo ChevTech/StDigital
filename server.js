452bceb6ca1014b187e42cd39f5068e702be4f5f
ChevTech <anton.stoytchev@gmail.com>
2018-05-01 23:23:53 -0400

    Test AWS CI/CD pipeline

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send(_.capitalize('Hello world!'))
+	res.send(_.capitalize('HelloWorld, aws CI/CD pipeline is configured for continuous deployment'))
 })
 
 app.listen(8081);
