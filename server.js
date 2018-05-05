a156698d34beebea2ec7ec85763d7bbbb0e2ce5f
ChevTech <anton.stoytchev@gmail.com>
2018-05-04 22:56:59 -0400

    Adding Manager Parameters

--- server.js
+++ server.js
@@ -1,46 +1,80 @@
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const AWS = require('aws-sdk');
 
-app.use(bodyParser.json());
+AWS.config.update({region: 'us-east-2'});
+const ssm = new AWS.SSM();
 
-app.get('/', function (req, res) {
-	console.log("HelloWorld");
-	res.send(_.capitalize('HelloWorld'))
-})
+const params = {
+	Names: [ 'PAGE_ACCESS_TOKEN', ],
+	WithDecryption: true
+}
 
-app.post('/webhook', (req, res) => {
-	let body = req.body;
+const parameters = ssm.getParameters(params).promise();
+
+parameters.then(params => {
+	const app = express();
+
+	if(params.Parameters.length == 0){
+		return Promise.reject('Unable to load aws parameters');	
+	}
+
+	app.locals.PAGE_ACCESS_TOKEN = getPageAccessToken(params.Parameters);
 	
-	if(body.object === 'page') {
-		body.entry.forEach(function(entry) {
-			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
-		});
-
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		res.sendStatus(404);
-	}
-})
-
-app.get('/webhook', (req, res) => {
-	let VARIFY_TOKEN = "asdfghjkl";
-
-	let mode = req.query['hub.mode'];
-	let token = req.query['hub.verify_token'];
-	let challenge = req.query['hub.challenge'];
-
-	if(mode && token){
-		if(mode === 'subscribe' && token === VARIFY_TOKEN) {
-			console.log('WEBHOOK_VERIFIED');
-			res.status(200).send(challenge);
+	app.use(bodyParser.json());
+	
+	app.get('/', function (req, res) {
+		console.log("HelloWorld");
+		res.send(_.capitalize('HelloWorld'))
+	})
+	
+	app.post('/webhook', (req, res) => {
+		let body = req.body;
+		
+		if(body.object === 'page') {
+			body.entry.forEach(function(entry) {
+				let webhook_event = entry.messaging[0];
+				console.log(webhook_event);
+			});
+	
+			res.status(200).send('EVENT_RECEIVED');
 		} else {
-			res.sendStatus(403);
+			res.sendStatus(404);
 		}
+	})
+	
+	app.get('/webhook', (req, res) => {
+		let VARIFY_TOKEN = "asdfghjkl";
+	
+		let mode = req.query['hub.mode'];
+		let token = req.query['hub.verify_token'];
+		let challenge = req.query['hub.challenge'];
+	
+		if(mode && token){
+			if(mode === 'subscribe' && token === VARIFY_TOKEN) {
+				console.log('WEBHOOK_VERIFIED');
+				res.status(200).send(challenge);
+			} else {
+				res.sendStatus(403);
+			}
+		}
+	
+	})
+	
+	console.log('Starting node server on 8081');
+	app.listen(8081);
+}, console.error)
+.catch(console.error);
+
+function getPageAccessToken(parameters) {	
+	const PAGE_ACCESS_TOKEN = parameters.filter((param) => {
+		return param.Name == 'PAGE_ACCESS_TOKEN';
+	});
+
+	if(PAGE_ACCESS_TOKEN.length != 1){
+		throw new Error('Unable to load PAGE_ACCESS_TOKEN');
 	}
 
-})
-
-app.listen(8081);
+	return PAGE_ACCESS_TOKEN[0];
+}
