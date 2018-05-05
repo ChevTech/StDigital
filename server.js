ce177fa1c0ca4289b527305006cbd9bfb4ef0379
ChevTech <anton.stoytchev@gmail.com>
2018-05-05 00:07:50 -0400

    Fetch user profile from FB

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const AWS = require('aws-sdk');
 
 AWS.config.update({region: 'us-east-2'});
 const ssm = new AWS.SSM();
 
+const facebook_api = {
+	getProfile(PSID, PAGE_ACCESS_TOKEN) { return `https://graph.facebook.com/v2.6/${PSID}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}` }
+}
+
 const params = {
-	Names: [ 'PAGE_ACCESS_TOKEN', ],
+	Names: [ 'PAGE_ACCESS_TOKEN', 'permissions' ],
 	WithDecryption: true
 }
 
@@ -15,13 +20,14 @@ const parameters = ssm.getParameters(params).promise();
 
 parameters.then(params => {
 	const app = express();
-
+	
 	if(params.Parameters.length == 0){
 		return Promise.reject('Unable to load aws parameters');	
 	}
 
-	app.locals.PAGE_ACCESS_TOKEN = getPageAccessToken(params.Parameters);
-	
+	app.locals.PAGE_ACCESS_TOKEN = getParameter(params.Parameters, 'PAGE_ACCESS_TOKEN');
+	app.locals.permissions = getParameter(params.Parameters, 'permissions');
+
 	app.use(bodyParser.json());
 	
 	app.get('/', function (req, res) {
@@ -35,7 +41,7 @@ parameters.then(params => {
 		if(body.object === 'page') {
 			body.entry.forEach(function(entry) {
 				let webhook_event = entry.messaging[0];
-				console.log(webhook_event);
+				processWebhookEvent(webhook_event);
 			});
 	
 			res.status(200).send('EVENT_RECEIVED');
@@ -59,7 +65,6 @@ parameters.then(params => {
 				res.sendStatus(403);
 			}
 		}
-	
 	})
 	
 	console.log('Starting node server on 8081');
@@ -67,14 +72,27 @@ parameters.then(params => {
 }, console.error)
 .catch(console.error);
 
-function getPageAccessToken(parameters) {	
-	const PAGE_ACCESS_TOKEN = parameters.filter((param) => {
-		return param.Name == 'PAGE_ACCESS_TOKEN';
+function getParameter(parameters, key) {	
+	const parameter = parameters.filter((param) => {
+		return param.Name == key;
 	});
 
-	if(PAGE_ACCESS_TOKEN.length != 1){
-		throw new Error('Unable to load PAGE_ACCESS_TOKEN');
+	if(parameter.length != 1){
+		throw new Error(`Unable to load ${key}`);
 	}
 
-	return PAGE_ACCESS_TOKEN[0];
+	return parameter[0];
+}
+
+async function processWebhookEvent(webhook_event){
+	const PAGE_ACCESS_TOKEN = app.locals.PAGE_ACCESS_TOKEN.value;
+	const permissions = app.locals.permissions.split(',');
+	const { first_name, last_name } = await fetch(facebook_api.getProfile(webhook_event.sender.id), app.locals.PAGE_ACCESS_TOKEN);
+	console.log(userProfile);
+
+	if(permissions.includes(`${first_name} ${last_name}`)){
+		console.log('fetching google image');
+	}
+
+	return;
 }
