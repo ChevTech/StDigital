4d9cb32c1b11e9338ce807975b39681e0d9d283b
ChevTech <anton.stoytchev@gmail.com>
2018-05-04 23:05:33 -0400

    Temp remove logging

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-/home/ec2-user/.nvm/versions/node/v8.11.1/bin/node /StDigital/server.js >my_app_log.log 2>&1 &
+/home/ec2-user/.nvm/versions/node/v8.11.1/bin/node /StDigital/server.js >/dev/null 2>&1 &
